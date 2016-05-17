0f0894e67391ecf6b1a563196ab1a84c518a56ff
Boloutare Doubeni <tarecd@gmail.com>
2016-05-17 16:42:24 -0400

    Add routes

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Query
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    def test_create_query(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('users')
+        data = {'name': 'DabApps'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Query.objects.count(), 1)
+        self.assertEqual(Query.objects.get().name, 'DabApps')
+
+
